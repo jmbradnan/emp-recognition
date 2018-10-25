ba36abafdf34f74ab7a84253898f2893b456c422
jtopgi <jtopgi@users.noreply.github.com>
2018-10-24 23:12:05 -0500

    add back ssl.

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ require("dotenv").config();
 
 const { Pool } = require("pg");
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
 });
 
 const http = require("http");
