d9e40ee1ee0b11ac8b0b225fca63969e118d0d9c
jtopgi <jtopgi@users.noreply.github.com>
2018-10-24 20:20:43 -0500

    prettify.

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL
 });
 
-const http = require('http');
-var moment = require('moment');
-var express = require('express');
-const path = require('path');
+const http = require("http");
+var moment = require("moment");
+var express = require("express");
+const path = require("path");
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("port", process.env.PORT || 5000);
+app.use(express.static(__dirname + "/public"));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.get('/homepage', async (req, res) => {
+app.get("/homepage", async (req, res) => {
   try {
-    const client = await pool.connect()
-    const result = await client.query('SELECT * FROM users');
-    const results = { 'results': (result) ? result.rows : null};
-    res.render('pages/homepage', results );
+    const client = await pool.connect();
+    const result = await client.query("SELECT * FROM users");
+    const results = { results: result ? result.rows : null };
+    res.render("pages/homepage", results);
     client.release();
   } catch (err) {
     console.error(err);
@@ -30,107 +30,122 @@ app.get('/homepage', async (req, res) => {
 });
 
 // Show abbreviated list (names) of all users
-app.get('/show-all-users', async (req, res) => {
-    try {
-      const client = await pool.connect()
-      const result = await client.query('SELECT * FROM users');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/administration', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error: " + err);
-    }
+app.get("/show-all-users", async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query("SELECT * FROM users");
+    const results = { results: result ? result.rows : null };
+    res.render("pages/administration", results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error: " + err);
+  }
 });
 
 // get user fields by id
-app.get('/get-user', async (req, res) => {
-  console.log(req.query.id)
-      try {
-      const client = await pool.connect()
-      var id = "id=" + req.query.id;
-      const queryResult = await client.query('SELECT * FROM users WHERE ' + id);
-      const results = { 'jsonData': (queryResult) ? queryResult.rows : null};
-      console.log(results);
-      res.send(results.jsonData);
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
+app.get("/get-user", async (req, res) => {
+  console.log(req.query.id);
+  try {
+    const client = await pool.connect();
+    var id = "id=" + req.query.id;
+    const queryResult = await client.query("SELECT * FROM users WHERE " + id);
+    const results = { jsonData: queryResult ? queryResult.rows : null };
+    console.log(results);
+    res.send(results.jsonData);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
 });
 
 // delete user
-app.get('/delete-user', async(req, res) => {
+app.get("/delete-user", async (req, res) => {
   try {
     const client = await pool.connect();
-    const queryResult = await client.query('DELETE FROM users WHERE id=($1)', [req.query.id]);
-    res.render('pages/administration');
+    const queryResult = await client.query("DELETE FROM users WHERE id=($1)", [
+      req.query.id
+    ]);
+    res.render("pages/administration");
     client.release();
   } catch (err) {
-        console.error(err);
+    console.error(err);
     res.send("Error: " + err);
   }
 });
 
 // update user info
-app.get('/update-user', async(req, res) => {
+app.get("/update-user", async (req, res) => {
   try {
     const client = await pool.connect();
-    const queryResult = await client.query('UPDATE users SET fname=($1), lname=($2), email=($3), password=($4) WHERE id=($5)',
-    [req.query.fname, req.query.lname, req.query.email, req.query.password, req.query.id]);
-    res.redirect('pages/administration');
+    const queryResult = await client.query(
+      "UPDATE users SET fname=($1), lname=($2), email=($3), password=($4) WHERE id=($5)",
+      [
+        req.query.fname,
+        req.query.lname,
+        req.query.email,
+        req.query.password,
+        req.query.id
+      ]
+    );
+    res.redirect("pages/administration");
     client.release();
   } catch (err) {
-        console.error(err);
+    console.error(err);
     res.send("Error: " + err);
   }
 });
 
 // add a new user
-app.post('/new-user',function(req,res) {
-  var data = [req.body.fname, req.body.lname, req.body.email, req.body.password];
+app.post("/new-user", function(req, res) {
+  var data = [
+    req.body.fname,
+    req.body.lname,
+    req.body.email,
+    req.body.password
+  ];
   console.log(data);
   // add new user to user table
-  var sql = 'INSERT INTO users (fname, lname, email, password) VALUES ($1, $2, $3, $4) RETURNING id';
+  var sql =
+    "INSERT INTO users (fname, lname, email, password) VALUES ($1, $2, $3, $4) RETURNING id";
   pool.query(sql, data, function(err, result) {
-	  console.log("in query");
-	  if (err) {
-		console.error(err);
-	}
-	console.log(result.rows[0].id);
-})
-res.redirect('/db');
+    console.log("in query");
+    if (err) {
+      console.error(err);
+    }
+    console.log(result.rows[0].id);
+  });
+  res.redirect("/db");
 });
 
 // get administration page
-app.get('/administration', async (req, res) => {
-    try {
-      const client = await pool.connect()
-      const result = await client.query('SELECT * FROM users');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/administration', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
+app.get("/administration", async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query("SELECT * FROM users");
+    const results = { results: result ? result.rows : null };
+    res.render("pages/administration", results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
 });
 
-app.get('/', (req, res) => res.render('pages/login'));
+app.get("/", (req, res) => res.render("pages/login"));
 
-app.use(function(req,res){
+app.use(function(req, res) {
   res.status(404);
-  res.render('404');
+  res.render("404");
 });
 
-app.use(function(err, req, res, next){
+app.use(function(err, req, res, next) {
   console.error(err.stack);
   res.status(500);
-  res.render('500');
+  res.render("500");
 });
 
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+app.listen(app.get("port"), function() {
+  console.log("Node app is running at localhost:" + app.get("port"));
+});
