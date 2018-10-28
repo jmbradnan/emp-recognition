b9f0e58aa544b0ad7dbf4fbdeee0be20ff9cc850
Jackie Bradnan <jmbradnan@gmail.com>
2018-10-28 13:35:29 -0700

    Jackie/signature and ux updates (#8)
    * cleaning up edit signature and refresh
    
    * commit before adding kickstart files
    
    * updating ux
    
    * more formatting of pages
    
    * add signature
    
    * remove local connection string
    
    * removed some uneeded css

--- public/js/administration.js
+++ public/js/administration.js
@@ -7,18 +7,52 @@
   Administration.js contains script to update users.
 */
 
+
 $(document).ready(function () {
   // selectClient();
 });
 
-function showUsers() {
-  fetch("/show-all-users").then(function(response) {
-    if(response.ok) {
-      console.log("ok");
-    } else {
-      console.log("error");
-    }
-  });
+var canvas = document.getElementById('signature-pad');
+
+// Adjust canvas coordinate space taking into account pixel ratio,
+// to make it look crisp on mobile devices.
+// This also causes canvas to be cleared.
+// function resizeCanvas() {
+//     // When zoomed out to less than 100%, for some very strange reason,
+//     // some browsers report devicePixelRatio as less than 1
+//     // and only part of the canvas is cleared then.
+//     var ratio =  Math.max(window.devicePixelRatio || 1, 1);
+//     canvas.width = canvas.offsetWidth * ratio;
+//     canvas.height = canvas.offsetHeight * ratio;
+//     canvas.getContext("2d").scale(ratio, ratio);
+// }
+
+// window.onresize = resizeCanvas;
+// resizeCanvas();
+
+var signaturePad = new SignaturePad(canvas, {
+  // backgroundColor: 'rgb(255, 255, 255)' // necessary for saving image as JPEG; can be removed is only saving as PNG or SVG
+});
+
+document.getElementById('save-png').addEventListener('click', function () {
+  if (signaturePad.isEmpty()) {
+    return alert("Please provide a signature first.");
+  }
+  
+  var data = signaturePad.toDataURL('image/png');
+  $("#user_signature_edit").val(data);
+  // console.log(data);
+  $("#signature_image").attr("src", data);
+  hideFields('#editable_user_signature');
+  showFields('#editable_user_fields');
+});
+
+function createSignature() {
+  var data = signaturePad.toDataURL('image/png');
+  $("#user_signature_initial").val(data);
+  hideFields('#enter_signature');
+  showFields('#hidden_signature');
+  $("#new_signature").attr("src", data);
 }
 
 /* 
@@ -51,6 +85,19 @@ function deleteUser() {
 }
 
 /*
+Display all users
+*/
+function showUsers() {
+  fetch("/show-all-users").then(function(response) {
+    if(response.ok) {
+      console.log("ok");
+    } else {
+      console.log("error");
+    }
+  });
+}
+
+/*
 Select a user
 */
 function selectUser(id) {
@@ -59,12 +106,16 @@ function selectUser(id) {
     if(response.ok) {
       response.json().then(function(json) {
         var data = json[0];
-        console.log(data);
+        // console.log(data);
         $("#user_id_edit").val(id);
         $("#user_fname_edit").val(data.fname);
         $("#user_lname_edit").val(data.lname);
         $("#user_email_edit").val(data.email);
         $("#user_password_edit").val(data.password);
+        console.log(data.signature);
+        $("#user_signature_edit").val(data.signature);
+
+        $("#signature_image").attr("src", data.signature);
       });
     } else {
       console.log("error");
@@ -72,20 +123,9 @@ function selectUser(id) {
   });
 }
 
-/*
-Update a user
-*/
-function updateUser() {
-  var fields = $("#editable_user_fields .input-field");
-  var id = $('#user_id_edit').val();
-  var query = "/update-user" + queryBuilder("id", id, fields);
-  fetch(query).then(function(response) {
-    if(response.ok) {
-      console.log("ok");
-    } else {
-      console.log("error");
-    }
-  });
+function updateSignature() {
+  hideFields('#editable_user_fields');
+  showFields('#editable_user_signature');
 }
 
 function searchForUser() {
@@ -98,6 +138,8 @@ function allUserDisplay() {
   hideFields('#new_user_fields');
   hideFields('#select_user');
   hideFields('#search_for_user');
+  hideFields('#editable_user_fields');
+  showUsers();
 }
 
 // Toggles display to appropriate fields for adding new client
@@ -106,6 +148,7 @@ function newUserDisplay(){
   hideFields('#all_user_fields');
   hideFields('#select_user');
   hideFields('#search_for_user');
+  hideFields('#editable_user_fields');
 }
 
 // Toggles display to appropriate fields for editing existing user
@@ -114,6 +157,7 @@ function editUserDisplay(){
   hideFields('#all_user_fields');
   hideFields('#new_user_fields');
   hideFields('#search_for_user');
+  hideFields('#editable_user_fields');
 }
 
 // Toggles Search controls
@@ -122,4 +166,5 @@ function searchUserDisplay() {
   hideFields('#all_user_fields');
   hideFields('#new_user_fields');
   hideFields('#select_user');
+  hideFields('#editable_user_fields');
 }
