df163a029cbe7d59e0ed49cad5c91fbc6fcd5900
Jeevan Topgi <jtopgi@users.noreply.github.com>
2018-11-10 19:14:41 -0600

    JT-10: Create User change name. (#17)
    * add change name page.
    
    * add test and edit route.
    
    * prettify

--- index.js
+++ index.js
@@ -161,6 +161,38 @@ app.post(
   }
 );
 
+//show change name form
+app.get("/user/name", ensureLoggedIn("/user/login"), async (req, res) => {
+  try {
+    const client = await pool.connect();
+    var result = await client.query("SELECT * FROM users WHERE id=($1)", [
+      req.user.id
+    ]);
+    const data = { user: result ? result.rows[0] : null };
+    res.render("pages/user/name", data);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error: " + err);
+  }
+});
+
+//change name
+app.post("/user/name/edit", ensureLoggedIn("/user/login"), async (req, res) => {
+  try {
+    const client = await pool.connect();
+    await client.query(
+      "UPDATE users SET fname=($1), lname=($2) WHERE id=($3)",
+      [req.body.fname, req.body.lname, req.user.id]
+    );
+    res.redirect("/user/name");
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error: " + err);
+  }
+});
+
 /*
  * Admin Routes
  */
@@ -381,8 +413,8 @@ app.get("/reset", async (req, res) => {
     var user = await client.query(
       "INSERT INTO users VALUES(DEFAULT, ($1), ($2), ($3), ($4)) RETURNING id",
       [
-        faker.name.findName(),
-        faker.name.findName(),
+        faker.name.firstName(),
+        faker.name.lastName(),
         "admin@admin.com",
         "password"
       ]
