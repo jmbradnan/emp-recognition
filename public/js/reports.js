c1fdee733e576a7c89bc91adb772e6903ae45c78
Jackie Bradnan <jmcgee@microsoft.com>
2018-11-10 19:43:27 -0800

    pie chart for who makes reports

--- public/js/reports.js
+++ public/js/reports.js
@@ -8,7 +8,7 @@ function awardsCreated() {
         // var data = json;
         console.log(json);
         // showReport(data);
-        drawChart(json);
+        getChartData(json);
       });
     } else {
       console.log("error");
@@ -16,35 +16,14 @@ function awardsCreated() {
   });
 }
 
-//   var url = "/get-user" + "?id=" + id;
-//   fetch(url).then(function(response) {
-//     if(response.ok) {
-//       response.json().then(function(json) {
-//         var data = json[0];
-//         // console.log(data);
-//         $("#user_id_edit").val(id);
-//         $("#user_fname_edit").val(data.fname);
-//         $("#user_lname_edit").val(data.lname);
-//         $("#user_email_edit").val(data.email);
-//         $("#user_password_edit").val(data.password);
-//         console.log(data.signature);
-//         $("#user_signature_edit").val(data.signature);
-
-//         $("#signature_image").attr("src", data.signature);
-//       });
-//     } else {
-//       console.log("error");
-//     }
-//   });
-// }
-
-function drawChart(json) {
+function getChartData(json) {
     var data = new google.visualization.DataTable();
   
     showFields('#report_area')
     data.addColumn('string', 'Name');
-    // data.addColumn('number', 'ID');
     data.addColumn('number', 'count');
+
+
     var size = Object.keys(json).length;
     var awardsCount = {};
     for (i=0; i<json.length; i++) {
@@ -54,9 +33,8 @@ function drawChart(json) {
         } else {
            awardsCount[id] += 1;
         }
-        console.log(awardsCount);
     }
-    var table = {};
+    var table = new Array();
     var keys = Object.keys(awardsCount);
     for (j=0; j<keys.length; j++) {
         var id = keys[j];
@@ -66,25 +44,28 @@ function drawChart(json) {
             response.json().then(function(json) {
                 var userData = json[0];
                 var name = userData.fname + " " + userData.lname;
-                var count = awardsCount[id];
-                table[name] = count;
+                var count = Number(awardsCount[userData.id]);
+                table.push([name, count]);
+
+                console.log(table);
+                if (table.length === keys.length) {
+                    data.addRows(table);
+                    // Set chart options
+                    var options = {'title':'Users that made awards',
+                       'width':400,
+                       'height':300};
+
+                // Instantiate and draw our chart, passing in some options.
+                var chart = new google.visualization.PieChart(document.getElementById('report'));
+                chart.draw(data, options);
+                }
                  });
             } else {
                 console.log("error");
             }
        });
-
     }
-
-// data.rows("User", id);
-    // Set chart options
-        // var options = {'title':'Users that made awards',
-        //                'width':400,
-        //                'height':300};
-
-        // // Instantiate and draw our chart, passing in some options.
-        // var chart = new google.visualization.BarChart(document.getElementById('report'));
-        // chart.draw(data, options);
+    
 }
 
 // function showReport(data) {
