b4e3a0cb0699b99a93e29402f50f7529422a7bde
Jackie Bradnan <jmcgee@microsoft.com>
2018-11-11 19:17:00 -0800

    bool for admin flag in user table

--- public/js/createuser.js
+++ public/js/createuser.js
@@ -2,6 +2,14 @@ var canvas = document.getElementById('signature-pad');
 var signaturePad = new SignaturePad(canvas, {
 });
 
+document.getElementById('administratorCheck').addEventListener('change', function () {
+    var state = document.getElementById('administratorCheck').checked;
+    var newState = state ? 'true' : 'false';
+    document.getElementById('administrator_check').value = newState
+  
+});
+
+
 function createSignature() {
   var data = signaturePad.toDataURL('image/png');
   $("#user_signature_initial").val(data);
