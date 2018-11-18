4f86cd01c1c1e10be94508334d609b89552f3c17
Jackie Bradnan <jmcgee@microsoft.com>
2018-11-18 10:57:59 -0800

    added more reports

--- public/js/reports.js
+++ public/js/reports.js
@@ -1,11 +1,9 @@
-
-
-function awardsCreated() {
-    var url = "/awardsCreatedReport";
+function displayAllAwards() {
+    var url = "/displayAllAwards";
     fetch(url).then(function(response) {
     if(response.ok) {
       response.json().then(function(json) {
-        getChartData(json);
+        getAllAwardsChart(json);
       });
     } else {
       console.log("error");
@@ -13,7 +11,125 @@ function awardsCreated() {
   });
 }
 
-function getChartData(json) {
+function awardsCreated() {
+    var url = "/awardsCreatedReport";
+    fetch(url).then(function(response) {
+    if(response.ok) {
+      response.json().then(function(json) {
+        getAwardsCreatedChart(json);
+      });
+    } else {
+      console.log("error");
+    }
+  });
+}
+
+function awardsOverTime() {
+    var url = "/awardsOverTimeReport";
+    fetch(url).then(function(response) {
+    if(response.ok) {
+      response.json().then(function(json) {
+        getAwardsOverTimeChart(json);
+      });
+    } else {
+      console.log("error");
+    }
+  });
+}
+
+function getAllAwardsChart(json) {
+    var data = new google.visualization.DataTable();
+    showFields('#report_area');
+    data.addColumn('string', 'Award From');
+    data.addColumn('string', 'Award To');
+    data.addColumn('string', 'Email');
+    data.addColumn('string', 'Award-Type');
+    data.addColumn('string', 'Time');
+    data.addColumn('string', 'Date');
+    var table = new Array();
+    for (i=0; i<json.length; i++) {
+        var sender = json[i]['user'];
+        var recipient = json[i]['name'];
+        var email = json[i]['email'];
+        var type = json[i]['type'];
+        var time = moment(json[i]['time'], 'HH:mm:ss').format('h:mm A');
+        var date = json[i]['date'].split("T");
+        var dateShortened = date[0];
+        table.push([sender, recipient, email, type, time, dateShortened]);
+    }
+     data.addRows(table);
+     // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.Table(document.getElementById('report'));
+    chart.draw(data, {showRowNumber: true, width: '100%', height: '100%'});
+}
+
+function getAwardsOverTimeChart(json) {
+    var data = new google.visualization.DataTable();
+    var months = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
+    showFields('#report_area');
+    data.addColumn('string', 'Date');
+    data.addColumn('number', 'Awards');
+    data.addColumn('number', 'Weekly');
+    data.addColumn('number', 'Monthly');
+    console.log(json);
+    var table = new Array();
+    var keys = [];
+    var awardsCount = {};
+    var typeCount = {};
+    for (i=0; i<json.length; i++) {
+        var returnedDate = json[i]['date'];
+        var date = returnedDate.split("T");
+        console.log(date[0]); 
+        var parts = date[0].split("-");
+        var month = parts[1];
+        var year = parts[0];
+        var id = month+year;
+        var type = json[i]['type'];
+        if (!awardsCount.hasOwnProperty(id)) {
+            keys.push(id);
+            if (type === "Month") {
+                awardsCount[id] = [1, 0, 1];
+            } else {
+                awardsCount[id] = [1, 1, 0];
+            }
+        } else {
+           awardsCount[id][0] += 1;
+           if (type === "Month") {
+               awardsCount[id][2] += 1;
+           } else {
+               awardsCount[id][1] += 1;
+           }
+        }
+        console.log(awardsCount);
+    }
+    // var keys = Object.keys(awardsCount);   // need these in date sorted order
+    for (j=0; j<keys.length; j++) {
+        var monthLabel = keys[j].substring(0,2).replace(/^[0|\D]*/,'');
+        var monthPosition = parseInt(monthLabel) -1;
+        var yearLabel = keys[j].substring(2,6);
+        var count = awardsCount[keys[j]][0];
+        var weekly = awardsCount[keys[j]][1];
+        var monthly = awardsCount[keys[j]][2];
+        var dateLabel = months[monthPosition] + " " + yearLabel;
+        console.log(monthLabel);
+        table.push([dateLabel, count, weekly, monthly]);
+        console.log(table);
+    }
+        data.addRows(table);
+        // Set chart options
+        var options = {'title':'Awards by month',
+          curveType: 'function',
+           'width':400,
+           'height':300,
+          legend: { position: 'bottom' }
+        };
+     // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.LineChart(document.getElementById('report'));
+    chart.draw(data, options);
+    
+}
+
+function getAwardsCreatedChart(json) {
     var data = new google.visualization.DataTable();
   
     showFields('#report_area')
