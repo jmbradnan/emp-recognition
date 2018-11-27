bed6349bf977a9e98d4f099d0ea6ecb8fca6ccdf
unknown <vicwang22@gmail.com>
2018-11-27 09:45:18 +0700

    changed module for email

--- latex/script02.js
+++ latex/script02.js
@@ -1,7 +1,11 @@
-//read electronic signature data URI from file and convert to image file
 const fs = require('fs');
 const imageDataURI = require('image-data-uri');
+const latex = require('node-latex');
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey('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');
+const base64 = require('file-base64');
 
+//create electronic signature image file - decode from data URI
 var dataURI = '';
 fs.readFile(__dirname + '/e_sig.txt', 'utf8', function read(err, data) {
 	if (err) {
@@ -13,11 +17,8 @@ fs.readFile(__dirname + '/e_sig.txt', 'utf8', function read(err, data) {
 	imageDataURI.outputFile(dataURI, filePath);
 })
 
-
-
+//create LaTex certicate
 //Node-Latex 
-const latex = require('node-latex');
-
 const input = fs.createReadStream(__dirname + '/certificate.tex'); //read in certificate template in laTex
 const output = fs.createWriteStream(__dirname + '/certificate.pdf'); //write certificate to pdf file
 
@@ -30,78 +31,38 @@ pdf = latex(input, options); //run pdflatex
 pdf.pipe(output);
 pdf.on('error', err => console.error(err));
 
-//setup for Nodemailer
-//get email addresses from files 
+//read in email addresses from files
 var email_sender = '';
 var email_recipient = '';
 
 fs.readFile(__dirname + '/email_sender.txt', 'utf8', function read(err, data) {
-	if (err) {
-		throw err;
-	}
-	email_sender = data;
+  if (err) {
+    throw err;
+  }
+  email_sender = data;
 })
 
 fs.readFile(__dirname + '/email_recipient.txt', 'utf8', function read(err, data) {
-	if (err) {
-		throw err;
-	}
-	email_recipient = data;
+  if (err) {
+    throw err;
+  }
+  email_recipient = data;
 })
 
-
-//Nodemailer
-'use strict';
-
-const nodemailer = require('nodemailer');
-
-// Generate test SMTP service account from ethereal.email
-// Only needed if you don't have a real mail account for testing
-nodemailer.createTestAccount((err, account) => {
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-        host: 'smtp.ethereal.email',
-        port: 587,
-        secure: false, // true for 465, false for other ports
-        auth: {
-            user: account.user, // generated ethereal user
-            pass: account.pass // generated ethereal password
-        }
-    });
-
-    // setup email data with unicode symbols
-    let mailOptions = {
-        from: email_sender, // sender address 
-        to: email_recipient, // list of receivers
-        subject: 'Employee Recognition Award', // Subject line
-        text: 'Here is your certificate.', // plain text body
-        html: '<b>Here is your certificate.</b>', //html body
-        attachments: [
-            { 
-                filename: 'certificate.pdf',
-                path: __dirname + '/certificate.pdf'
-            }
-        ]
-    };
-
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message sent: %s', info.messageId);
-        // Preview only available when sending through an Ethereal account
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-        
-        // save preview URL to file
-        var messageInfo = nodemailer.getTestMessageUrl(info);
-        const fs = require('fs');
-        fs.writeFile(__dirname + "/preview_URL.txt", messageInfo, function(err) {
-            if(err) {
-                return console.log(err);
-            }
-        });     
-    });
+//prepare email attachment- encode file to base64 string
+//send email via SendGrid
+base64.encode('certificate.pdf', function(err, base64String) {
+  const msg = {
+    to: email_recipient,
+    from: email_sender,
+    subject: 'Employee Recognition Award',
+    html: '<p>Here’s your certificate!</p>',
+    attachments: [
+      {
+        content: base64String,
+        filename: 'certificate.pdf'
+      }
+    ]
+  };
+  sgMail.send(msg);
 });
